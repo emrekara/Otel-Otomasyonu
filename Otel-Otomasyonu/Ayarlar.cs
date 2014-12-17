35343ffacd322d552868ee9a61b6e51eb0a0595d
Emre Kara <e_kara_e@hotmail.com>
2014-12-17 15:30:52 +0200

    First Commit
    Oluşturma

---
+++ Otel-Otomasyonu/Ayarlar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Otel_Otomasyonu
+{
+    class Ayarlar
+    {
+
+        public static SqlConnection baglanti;
+
+        public static void BaglantiAc()
+        {
+            baglanti = new SqlConnection("SERVER=EmreKara\\EMRE; Database=Otel; Integrated Security=true;");
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+
+        }
+
+        public static void KullaniciGiris(string k_adi, string sifre)
+        {
+            DataTable veriler = new DataTable();
+            string kullanici_adi = k_adi, kullanici_sifre = sifre;
+            BaglantiAc();
+            string asd="select PersonelTCNo from PersonelBilgi where PersonelTCNo like '"+kullanici_adi+"'";
+            SqlCommand kullanici_adi_sorgu = new SqlCommand(asd, baglanti);
+            
+
+            if (kullanici_adi == "")
+            {
+                MessageBox.Show(kullanici_adi_sorgu.ToString());
+            }
+            else
+            {
+                MessageBox.Show("sads");
+            }
+        }
+
+        public static void BaglantiKapat()
+        {
+            baglanti.Close();
+        }
+
+    }
+}
---
+++ Otel-Otomasyonu/GirisSayfasi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Otel_Otomasyonu 
+{
+    public partial class GirisSayfasi : Form
+    {
+        public GirisSayfasi()
+        {
+            InitializeComponent();
+        }
+
+        private void GirisSayfasi_Load(object sender, EventArgs e)
+        {
+            Sifre.Enabled = false;
+            Giris.Enabled = false;
+            KullaniciAdi.Focus();
+        }
+
+        private void KullaniciAdi_TextChanged(object sender, EventArgs e)
+        {
+            if (KullaniciAdi.Text.Length<11)
+            {
+                Sifre.Enabled = true;
+                Giris.Enabled = true;
+
+            }
+            else
+            {
+                Sifre.Enabled = false;
+                Giris.Enabled = false;
+            }
+        }
+
+        private void Giris_Click(object sender, EventArgs e)
+        {
+            Ayarlar.KullaniciGiris(KullaniciAdi.Text, Sifre.Text);   
+        }
+    }
+}
